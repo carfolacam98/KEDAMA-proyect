a4814f53313d868b7680cffc139b38dd691288f9
carfolacam98 <laura.carfolacam@gmail.com>
2021-10-25 14:08:08 -0500

    condigured server production front

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 const callApi = async (url, reqOptions) => {
   if (url === "/products/all") {
-    var response = await fetch("http://localhost:3001/api" + url);
+    var response = await fetch("https://minticappbackdivide.herokuapp.com/api" + url);
   } else {
-    response = await fetch("http://localhost:3001/api" + url, reqOptions);
+    response = await fetch("https://minticappbackdivide.herokuapp.com/api" + url, reqOptions);
   }
   const data = await response.json();
   return data;
